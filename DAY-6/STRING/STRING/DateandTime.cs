b9634f766819949d7356ee72c6c3ead33c61c560
Sivalingam K <sivalingam.k@ctlep.local>
2024-08-14 18:00:34 +0530

    String Concept

---
+++ DAY-6/STRING/STRING/DateandTime.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace STRING
+{
+    internal class DateandTime
+    {
+        static void Main(string[] args)
+        {
+           
+
+            //assigns default value 01/01/0001 00:00:00
+            DateTime dt1 = new DateTime();
+            Console.WriteLine("default Time : " + dt1);
+            Console.WriteLine();
+            //assigns year, month, day
+            DateTime dt2 = new DateTime(2024, 08, 14);
+            Console.WriteLine(" Year-Month-Day : " + dt2);
+            Console.WriteLine();
+            //assigns year, month, day, hour, min, seconds
+            DateTime dt3 = new DateTime(2024, 08, 14, 5, 10, 20);
+            Console.WriteLine(" Year-Month-Day hour-min-sec : " + dt3);
+            Console.WriteLine();
+
+            //Static Dates
+            DateTime currentDateTime = DateTime.Now;  //returns current date and time
+            Console.WriteLine("Current Date And Time :" +currentDateTime);
+            Console.WriteLine();
+            DateTime todaysDate = DateTime.Today; // returns today's date
+            Console.WriteLine("Today Date : " +todaysDate);
+            Console.WriteLine();
+            DateTime currentDateTimeUTC = DateTime.UtcNow;// returns current UTC date and time
+            Console.WriteLine("Utc Current Date and Time :"+currentDateTimeUTC);
+            Console.WriteLine();
+            DateTime maxDateTimeValue = DateTime.MaxValue; // returns max value of DateTime
+            Console.WriteLine("Max Date Time Value : " +maxDateTimeValue);
+            Console.WriteLine();
+            DateTime minDateTimeValue = DateTime.MinValue; // returns min value of DateTime
+            Console.WriteLine("min Date Time Value : "+minDateTimeValue);
+            Console.WriteLine();
+
+
+            //Timespan
+
+            DateTime dt = new DateTime(2015, 12, 31);
+
+            TimeSpan ts = new TimeSpan(25, 20, 55);
+
+            DateTime newDate = dt.Add(ts);
+
+            Console.WriteLine(newDate);//1/1/2016 1:20:55 AM
+
+
+            //Convert String to DateTime
+            var str = "5/12/2020";
+            DateTime dts;
+
+            var isValidDate = DateTime.TryParse(str, out dts);
+
+            if (isValidDate)
+                Console.WriteLine(dts);
+            else
+                Console.WriteLine($"{str} is not a valid date string");
+        }
+    }
+}
---
+++ DAY-6/STRING/STRING/Program.cs
@@ -0,0 +1,52 @@
+﻿internal class Program
+{
+    //private static void Main(string[] args)
+    //{
+    //    Console.Write("Enter the First String : ");
+
+    //    String str1 = Console.ReadLine();
+    //    Console.WriteLine();
+    //    Console.Write("Enter the Second String : ");
+
+    //    String str2 = Console.ReadLine();
+    //    Console.WriteLine("--------------------------------------------------------------");
+       
+    //    //String INterpolation
+    //    Console.WriteLine("InterPolation Method : " +$"Welcome {str1}{str2}") ;
+    //    //1.ToCharArray
+    //    char[] chars = str1.ToCharArray();
+    //    Console.WriteLine("ToCharArray Method :");
+    //    for(int i=0; i < chars.Length; i++)
+    //    {
+    //        Console.WriteLine(chars[i]);
+    //    }
+      
+
+    //    //2.Length
+    //    Console.WriteLine("Length : " + str1.Length);
+
+    //    //3.Concat
+
+    //    String cc=String.Concat(str1,str2);
+    //    Console.WriteLine("String Concat : "+ cc);
+
+    //   //4.Equals
+    //   Boolean isEqual=str1.Equals(str2);
+    //    Console.WriteLine(isEqual);
+
+    //    //5.Immutability
+    //    String str = "Hello";
+    //    str = string.Concat(str, "World");
+    //    Console.WriteLine("Immutability : " + str);
+
+    //    // use the escape character
+    //    string str3 = "This is the \"String\" class.";
+    //    //for(int i = 0; i < str3.Length; i ++)
+    //    //{
+    //    //    Console.WriteLine(str3[i]);
+    //    //}
+    //    Console.WriteLine("Escape Character : " + str3);
+
+
+    //}
+}
